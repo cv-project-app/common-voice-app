ff88057dbc0bd2cf0161c96332ec371a2ac4b7ee
razmikb <badalyanrazmik@gmail.com>
2025-05-20 21:39:38 +0400

    Update StatsService.kt

--- lib/src/main/java/org/commonvoice/saverio_lib/api/services/StatsService.kt
+++ lib/src/main/java/org/commonvoice/saverio_lib/api/services/StatsService.kt
@@ -44,6 +44,12 @@ interface StatsService {
         @Query("end_date") end_date: String?
     ): Response<ResponseAppUsage>
 
+    @GET("https://commonvoice.mozilla.org/api/v1/stats/languages") // Absolute URL to Mozilla API
+suspend fun getMozillaLanguageStats(): Response<List<MozillaLanguageStat>>
+
+@GET("v2/messages/get/")
+suspend fun getNewMessages(): Response<Map<String, Message>>
+    
     @GET("v2/messages/get/")
     suspend fun getNewMessages(): Response<Map<String, Message>>
 
