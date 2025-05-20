c6e99292cf6ac26121d07468973e2eb2b55037dd
razmikb <badalyanrazmik@gmail.com>
2025-05-20 21:37:21 +0400

    Update StatsService.kt

--- lib/src/main/java/org/commonvoice/saverio_lib/api/services/StatsService.kt
+++ lib/src/main/java/org/commonvoice/saverio_lib/api/services/StatsService.kt
@@ -4,6 +4,7 @@ import org.commonvoice.saverio_lib.api.requestBodies.RetrofitStatsUpdate
 import org.commonvoice.saverio_lib.api.requestBodies.RetrofitUserAppUsageBody
 import org.commonvoice.saverio_lib.api.responseBodies.ResponseAppUsage
 import org.commonvoice.saverio_lib.api.responseBodies.ResponseDailyUsage
+import org.commonvoice.saverio_lib.api.responseBodies.MozillaLanguageStat
 import org.commonvoice.saverio_lib.models.Message
 import retrofit2.Response
 import retrofit2.http.*
